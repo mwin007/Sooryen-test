652c39218923727b35bffe1c79cf6fdb5b344bf8
Martin Nguyen <nguyen0martin@gmail.com>
2019-06-05 15:01:45 -0400

    Save to DB codes.

--- app/code/Sooryen/Test/Observer/Test.php
+++ app/code/Sooryen/Test/Observer/Test.php
@@ -4,19 +4,21 @@ namespace Sooryen\Test\Observer;
 class Test implements \Magento\Framework\Event\ObserverInterface
 {
   protected $_logger;
+  protected $_testFactory;
   public function __construct(
   \Psr\Log\LoggerInterface $logger, //log injection
+  \Sooryen\Test\Model\TestFactory $testFactory,
   array $data = []
   ) {
   $this->_logger = $logger;
-     // parent::__construct($data);
+  $this->_testFactory = $testFactory;
+  //parent::__construct($data);
   }
 
 
   public function execute(\Magento\Framework\Event\Observer $observer)
   {
-     //$order= $observer->getData('order');
-   //$order->doSomething();
+
     //order id
     $order = $observer->getEvent()->getOrder();
     $order_id = $order->getIncrementId();
@@ -30,9 +32,46 @@ class Test implements \Magento\Framework\Event\ObserverInterface
       $this->_logger->info($item->getProductId());
     }
 
+    $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    $product = $objectManager->create('Magento\Catalog\Model\Product')->load($item->getProductId());
+    $productId = $item->getProductId();
+    $this->_logger->info($product->getName()); //Get Product Name
+    $this->_logger->info($product->getSku()); //Get Product Name
+
     //custoemr id
     $customerId = $order->getCustomerId();
     $this->_logger->info($customerId);
 
+    $customerRepository = $objectManager->create(\Magento\Customer\Api\CustomerRepositoryInterface::class);
+    $customer = $customerRepository->getById($customerId);
+    if($customer->getId()){
+        $this->_logger->info($customer->getFirstname());
+        $this->_logger->info($customer->getLastname());
+        $customerFirst = $customer->getFirstname();
+        $customerLast = $customer->getLastname();
+    }
+
+    $test = $this->_testFactory->create();
+    $test->setOrderId($order_id);
+    $test->setCustomerId($customerId);
+    $test->save();
+
+    /*
+    foreach($items as $item) {
+      $productId = $item->getProductId();
+      $product = $objectManager->create('Magento\Catalog\Model\Product')->load($productId);
+      $productName = $product->getName();
+      $test->setProductName($productName);
+      $test->setOrderId($order_id);
+      $test->setCustomerId($customerId);
+      $test->setFirstName($customerFirst);
+      $test->setLastName($customerLast);
+
+    }
+    */
+
+
   }
 }
+
+
